79a94b334836fae82d99a0b52c15d1e40e119d62
Wagner Xu <wagnerhsu@hotmail.com>
2021-09-12 10:10:19 +0800

    Pass build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,6 +6,18 @@ using System.Windows;
 using Cinchoo.Core.Shell;
 using ChoEazyCopy;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChoEazyCopy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChoEazyCopy")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Cinchoo Inc")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -29,4 +41,18 @@ using ChoEazyCopy;
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
 )]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.19")]
+[assembly: AssemblyFileVersion("1.0.0.19")]
 [assembly: ChoShellFileAssociation(AppHost.AppFileExt)]
